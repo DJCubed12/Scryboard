a2139f3fae5a3ceeb7a98f6d8ea3b38c5efefd42
DJCubed12 <djcubed12@gmail.com>
2025-04-10 22:23:05 -0500

    Display card search images

--- frontend/src/app/admin-page/card-search/card-search.component.ts
+++ frontend/src/app/admin-page/card-search/card-search.component.ts
@@ -9,16 +9,32 @@ import { CardAPIService } from 'src/app/services/external-api.service';
 })
 export class CardSearchComponent {
   public results: any = undefined;
+  public cards: any = [];
 
   constructor(private readonly cardAPI: CardAPIService) {}
 
   public searchByTitle(cardTitle: string) {
     this.cardAPI.searchByTitle(cardTitle).subscribe(
-      (response) => (this.results = response),
+      (response) => {
+        this.results = response;
+        this.cards = response['data'];
+      },
       (error) => {
         console.error(error);
         this.results = error;
       }
     );
+  }
+
+  public getCardImage(card: any): string | null {
+    try {
+      const image_url = card?.image_uris?.small;
+      if (image_url) {
+        return image_url;
+      }
+    } catch (TypeError) {
+      // Card response doesn't follow normal format
+    }
+    return null;
   }
 }
