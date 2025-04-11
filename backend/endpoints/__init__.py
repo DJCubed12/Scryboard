94fb5e3fbd3dad10466dccea8c6ee56c880dfc7b
Donald Filbeck <drfff9@umsystem.edu>
2025-04-10 22:26:33 -0500

    Added rfid mock script

--- backend/endpoints/__init__.py
+++ backend/endpoints/__init__.py
@@ -1,3 +1,3 @@
 """Import all files with endpoints here. `app.py` imports this file to easily import all endpoints."""
 
-from . import examples
+from . import mat_endpoints
--- tools/mat_emulator.py
+++ tools/mat_emulator.py
@@ -1,10 +1,1 @@
-import requests, random
-
-backend_url = "http://localhost:5000/rfid"
-
-rand_rfid_tag = random.getrandbits(64)
-rfid_object = {'rfid': bytes.hex(rand_rfid_tag.to_bytes(8))}
-
-req = requests.post(backend_url, json = rfid_object)
-
-print(req.text)
+# TODO
