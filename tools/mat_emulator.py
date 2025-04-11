3d9cad3af799d4432ff9e708d08a88c814abbf2f
Donald Filbeck <drfff9@umsystem.edu>
2025-04-10 22:33:17 -0500

    Fixed mock script

--- tools/mat_emulator.py
+++ tools/mat_emulator.py
@@ -1,1 +1,9 @@
-# TODO
+import requests, random
+
+backend_url = "http://localhost:5000/rfid"
+
+rfid_object = {'rfid': bytes.hex(random.getrandbits(64).to_bytes(8))}
+
+req = requests.post(backend_url, json = rfid_object)
+
+print(req.text)
