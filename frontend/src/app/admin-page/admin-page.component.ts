e9246e7f114ea179229155a33d3504b1de20e3f9
DJCubed12 <djcubed12@gmail.com>
2025-05-07 12:53:19 -0500

    Listen for mat update events in Admin page

--- frontend/src/app/admin-page/admin-page.component.ts
+++ frontend/src/app/admin-page/admin-page.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
-import { CardAPIService } from 'src/app/services/card-api.service';
-import { GameStateService } from 'src/app/services/game-state.service';
+import { MatListenerService } from '../services/mat-listener.service';
 
 @Component({
   selector: 'admin-page',
@@ -12,13 +10,7 @@ import { GameStateService } from 'src/app/services/game-state.service';
 export class AdminPageComponent {
   public mats: string[] = [];
 
-  constructor(private readonly gameStateService: GameStateService) {
-    this.refreshMatList();
-  }
-
-  public refreshMatList() {
-    this.gameStateService
-      .getMats()
-      .subscribe((matList) => (this.mats = matList));
+  constructor(private readonly matListener: MatListenerService) {
+    this.matListener.getMats$().subscribe((matList) => (this.mats = matList));
   }
 }
