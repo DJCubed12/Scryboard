a608c059e617cba66871051a01a3d000050b7726
Donald Filbeck <drfff9@umsystem.edu>
2025-05-07 00:40:19 -0500

    Updated mat_emulator to send full set of rfids for the selected mat

--- tools/mat_emulator.py
+++ tools/mat_emulator.py
@@ -1,68 +1,94 @@
-import requests, random
+import requests, random, json
 from tkinter import *
 from tkinter import ttk
 
-backend_url = "http://localhost:5000/card"
-mat_sent_RFID = dict()
+backend_url = "http://localhost:5000"
+mats = dict()
+
+ALL_ZONES = ['Battlefield', 'Command', 'Library', 'Graveyard', 'Exile', 'Not Specified']
 
 
-def send_rfid_post(matid, rfid):
-    rfid_object = {"mat_id": matid, "rfid": rfid}
-    req = requests.post(backend_url, json=rfid_object)
+# def send_rfid_post(matid, rfid):
+#     rfid_object = {"mat_id": matid, "rfid": rfid}
+#     req = requests.post(backend_url, json=rfid_object)
+#     print(req.text)
+
+
+def send_full_post(matid):
+    backend_replace_mat = (backend_url + "/cards/<mat_id>/replace").replace("<mat_id>", matid)
+    json_object = []
+    for zone in mats[matid]:
+        for rfid in zone:
+            json_object += {"rfid": rfid, "mat_id": matid, "zone": zone}
+    req = requests.post(backend_replace_mat, json=json_object)
     print(req.text)
 
 
 def new_mat():
     matid = bytes.hex(random.getrandbits(64).to_bytes(8))
     mat_listbox.insert(END, matid)
-    mat_sent_RFID[matid] = []
+    mats[matid] = [[] for zone in ALL_ZONES]
 
 
-def send_rand_rfid():
-    selection = mat_listbox.curselection()
-    if not selection:
-        return
-    index = selection[0]
-    matid = mat_listbox.get(index)
-    rfid = bytes.hex(random.getrandbits(64).to_bytes(8))
-    mat_sent_RFID[matid].append(rfid)
-    rfid_listbox.insert(END, rfid)
-    send_rfid_post(matid, rfid)
-
-
-def resend_rfid():
+def add_rand_rfid():
     matsel = mat_listbox.curselection()
-    rfidsel = rfid_listbox.curselection()
-    if not matsel or not rfidsel:
+    zonesel = zone_listbox.curselection()
+    if not matsel or not zonesel:
         return
-    matindex = matsel[0]
-    rfidindex = rfidsel[0]
-    matid = mat_listbox.get(matindex)
-    rfid = rfid_listbox.get(rfidindex)
-    send_rfid_post(matid, rfid)
+    matid = mat_listbox.get(matsel[0])
+    zone = zone_listbox.get(zonesel[0])
+    rfid = bytes.hex(random.getrandbits(64).to_bytes(8))
+    mats[matid][ALL_ZONES.index(zone)].append(rfid)
+    rfid_listbox.insert(END, rfid)
+
+
+def remove_rfid():
+    matsel = mat_listbox.curselection()
+    zonesel = zone_listbox.curselection()
+    rfidsel = rfid_listbox.curselection()
+    if not matsel or not zonesel or not rfidsel:
+        return
+    matid = mat_listbox.get(matsel[0])
+    zone = zone_listbox.get(zonesel[0])
+    rfid = rfid_listbox.get(rfidsel[0])
+    mats[matid][ALL_ZONES.index(zone)].remove(rfid)
+    rfid_listbox.delete(rfidsel)
+    btn_rem_rfid["state"] = DISABLED
+
+
+def send_post():
+    matsel = mat_listbox.curselection()
+    if not matsel:
+        return
+    matid = mat_listbox.get(matsel[0])
+    send_full_post(matid)
 
 
 def onselect_rfid(event):
-    box = event.widget
-    selection = box.curselection()
-    if not selection:
-        btn_resend_rfid["state"] = DISABLED
+    rfidsel = rfid_listbox.curselection()
+    if not rfidsel:
+        btn_rem_rfid["state"] = DISABLED
     else:
-        btn_resend_rfid["state"] = NORMAL
+        btn_rem_rfid["state"] = NORMAL
 
 
-def onselect_mat(event):
-    box = event.widget
-    selection = box.curselection()
-    if not selection:
+def refresh_rfid():
+    matsel = mat_listbox.curselection()
+    zonesel = zone_listbox.curselection()
+    if not matsel or not zonesel:
         return
-    index = selection[0]
-    matid = box.get(index)
-    print("Selected: " + matid)
-    btn_new_rfid["state"] = NORMAL
+    matid = mat_listbox.get(matsel[0])
+    zone = zone_listbox.get(zonesel[0])
     rfid_listbox.delete(0, END)
-    for rfid in mat_sent_RFID[matid]:
+    for rfid in mats[matid][ALL_ZONES.index(zone)]:
         rfid_listbox.insert(END, rfid)
+
+def onselect_zone(event):
+    refresh_rfid()
+    
+def onselect_mat(event):
+    btn_new_rfid["state"] = NORMAL
+    refresh_rfid()
 
 
 root = Tk()
@@ -75,16 +101,24 @@ mat_listbox = Listbox(frm, selectmode=SINGLE, exportselection=False)
 mat_listbox.grid(column=0, row=1)
 mat_listbox.bind("<<ListboxSelect>>", onselect_mat)
 
+zone_listbox = Listbox(frm, selectmode=SINGLE, exportselection=False)
+zone_listbox.grid(column=1, row=1)
+zone_listbox.bind("<<ListboxSelect>>", onselect_zone)
+for zone in ALL_ZONES:
+    zone_listbox.insert(END, zone)
+zone_listbox.select_set(0)
+
 rfid_listbox = Listbox(frm, selectmode=SINGLE)
-rfid_listbox.grid(column=1, row=1, columnspan=2)
+rfid_listbox.grid(column=2, row=1, columnspan=2)
 rfid_listbox.bind("<<ListboxSelect>>", onselect_rfid)
 
-btn_new_rfid = Button(
-    frm, text="Send Rand RFID", command=send_rand_rfid, state=DISABLED
-)
-btn_new_rfid.grid(column=1, row=0)
+btn_new_rfid = Button(frm, text="Add RFID", command=add_rand_rfid, state=DISABLED)
+btn_new_rfid.grid(column=2, row=0)
 
-btn_resend_rfid = Button(frm, text="Resend RFID", command=resend_rfid, state=DISABLED)
-btn_resend_rfid.grid(column=2, row=0)
+btn_rem_rfid = Button(frm, text="Rem RFID", command=remove_rfid, state=DISABLED)
+btn_rem_rfid.grid(column=3, row=0)
+
+btn_send_post = Button(frm, text="Send Mat RFIDs", command=send_post, state=DISABLED)
+btn_send_post.grid(column=2, row=2, columnspan=2)
 
 root.mainloop()
