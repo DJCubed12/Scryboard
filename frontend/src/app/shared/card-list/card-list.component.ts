fe2e26698c06b0686fa125b2f5441702a44e767b
DJCubed12 <djcubed12@gmail.com>
2025-05-07 18:30:26 -0500

    Flip button directly in admin page

--- frontend/src/app/shared/card-list/card-list.component.ts
+++ frontend/src/app/shared/card-list/card-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Card } from 'src/app/models/card';
+import { GameStateService } from 'src/app/services/game-state.service';
 
 @Component({
   selector: 'card-list',
@@ -11,4 +12,12 @@ export class CardListComponent {
   @Input() cards: Card[] = [];
   @Input() allowEdit: boolean = false;
   @Input() debug: boolean = false;
+
+  constructor(private readonly gameStateService: GameStateService) {}
+
+  public flip(card: Card) {
+    if (this.allowEdit) {
+      this.gameStateService.flipCard(card.rfid, !card.is_face_up).subscribe();
+    }
+  }
 }
