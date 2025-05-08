6a23b6bda11b45fb7522dc27adfe1c41843f2cb4
DJCubed12 <djcubed12@gmail.com>
2025-05-07 21:58:07 -0500

    Underline button of current page

--- frontend/src/app/shared/top-bar/top-bar.component.ts
+++ frontend/src/app/shared/top-bar/top-bar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent {
+  public onMainPage: boolean = true;
 
+  constructor(private readonly router: Router) {
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.onMainPage = event.url === '/';
+        console.log(event);
+      });
+  }
 }
