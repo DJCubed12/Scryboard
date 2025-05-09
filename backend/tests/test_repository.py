79c12b22ababd39bdead989fedcc1a311ba135ab
DJCubed12 <djcubed12@gmail.com>
2025-05-09 17:20:12 -0500

    Remove debug test

--- backend/tests/test_repository.py
+++ backend/tests/test_repository.py
@@ -10,7 +10,3 @@ def test_is_empty(repository: CardRepository):
 def test_add_card(repository: CardRepository):
     repository.add_card("test", "test")
     assert len(repository.get_all()) == 1
-
-
-def test_still_empty(repository: CardRepository):
-    assert repository.get_all() == []
